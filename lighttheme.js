ac9ed6166bdbd67f715853059a891a50b0beb531
chinovnick <gagahahaha663@gmail.com>
2026-04-17 18:29:47 +0300

    first commit

---
+++ lighttheme.js
@@ -0,0 +1,3 @@
+function button() {
+    document.body.classList.toggle("light");
+}
---
+++ script.js
@@ -0,0 +1,46 @@
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 150) {
+        header.classList.add("header_scrolled");
+    } else {
+        header.classList.remove("header_scrolled");
+    }
+});
+
+//menu
+
+const menuBtn = document.getElementById("menu-button");
+const closeBtn = document.getElementById("close-button");
+const navMenu = document.getElementById("nav-menu");
+
+menuBtn.addEventListener("click", () => {
+    navMenu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+});
+
+//modal
+
+function selectButton() {
+  document.getElementById("select").style.display = "flex";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closeButton() {
+  document.getElementById("select").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+
+
+const modal = document.getElementById("select");
+const cancelBtn = document.getElementById("cancel");
+
+
+cancelBtn.onclick = () => {
+    modal.style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+};
+
